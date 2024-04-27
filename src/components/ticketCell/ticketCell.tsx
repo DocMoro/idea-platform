a3879ac824f92d5c7c0a313c1a414d8be28130f9
Andrey Morogin <andreandreandre10071998@yandex.ru>
2024-04-27 21:49:29 +0300

    feat: add ticket

--- src/components/ticketCell/ticketCell.tsx
+++ src/components/ticketCell/ticketCell.tsx
@@ -1,32 +1,83 @@
 import { FC } from 'react'
 
 import s from './ticketCell.module.scss'
+import { TTicket } from '../../constants/type'
+import { dayName, monthName } from '../../constants/constants'
 
-const ticket = {
-  origin: 'VVO',
-  origin_name: 'Владивосток',
-  destination: 'TLV',
-  destination_name: 'Тель-Авив',
-  departure_date: '12.05.18',
-  departure_time: '16:50',
-  arrival_date: '12.05.18',
-  arrival_time: '23:35',
-  carrier: 'SU',
-  stops: 1,
-  price: 16700
+type TicketCellProps = {
+  ticket: TTicket
 }
 
-export const TicketCell: FC = () => {
+export const TicketCell: FC<TicketCellProps> = ({ ticket }) => {
+  const {
+    price,
+    departure_time,
+    stops,
+    arrival_time,
+    origin,
+    origin_name,
+    destination,
+    destination_name,
+    arrival_date,
+    departure_date
+  } = ticket
+
+  const getStopsString = () => {
+    const check = stops % 10
+    let result
+
+    if (check > 4 || check === 0) result = 'пересадок'
+    else if (check > 1) result = 'пересадки'
+    else result = 'пересадка'
+
+    return `${stops} ${result}`
+  }
+
+  const getDateString = (dateStr: string) => {
+    const year = +('20' + dateStr.slice(6))
+    const month = parseInt(dateStr.slice(4, 6))
+    const day = parseInt(dateStr.slice(0, 3))
+
+    const date = new Date(year, month, day)
+
+    return `${day} ${monthName[month - 1]} ${year}, ${dayName[date.getDay()]}`
+  }
+
+  const getPriceString = () => {
+    const thousands = Math.floor(price / 1000)
+
+    return `${thousands || ''} ${price % 1000}`
+  }
+
   return (
     <li className={s.ticket}>
-      <div className={s.ticket__container}>
-        <div className={s.ticket__logo}></div>
-        <button className={s.ticket__button}>
+      <div className={s.left}>
+        <div className={s.logo}></div>
+        <button className={s.button}>
           Купить <br />
-          за {ticket.price}₽
+          за {getPriceString()}₽
         </button>
       </div>
-      <div></div>
+      <div className={s.right}>
+        <div className={s.container}>
+          <p className={s.time}>{departure_time}</p>
+          <div className={s.stopsContainer}>
+            <p className={s.stops}>{getStopsString()}</p>
+            <div className={s.airplane}></div>
+          </div>
+          <p className={s.time}>{arrival_time}</p>
+        </div>
+        <div className={s.container}>
+          <div className={s.textContainer}>
+            <p className={s.text}>{`${origin}, ${origin_name}`}</p>
+            <p className={s.date}>{getDateString(departure_date)}</p>
+          </div>
+          <div className={s.textContainer}>
+            <p className={s.text}>{`${destination_name}, ${destination}`}</p>
+            <p className={s.date}>{getDateString(arrival_date)}</p>
+          </div>
+        </div>
+      </div>
     </li>
   )
 }
